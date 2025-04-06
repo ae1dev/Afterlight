0b29303e1a416d25b7166364d4959de5d5fa4192
Elias <elias@deuss.dev>
2025-04-06 19:22:11 +0200

    feat(main): add background color

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Afterlight',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        scaffoldBackgroundColor: Color.fromARGB(255, 4, 9, 24),
       ),
       home: const HomeView(),
     );
