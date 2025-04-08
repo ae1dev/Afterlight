b84fa1be2c4d3c97ed8edf174ca79c99aaec4c43
Elias <elias@deuss.dev>
2025-04-08 13:31:50 +0200

    feat(home): simple list for testing

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,4 +1,8 @@
+import 'package:afterlight/main.dart';
+import 'package:afterlight/ui/settings/settings.dart';
+import 'package:apps_handler/apps_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,8 +12,51 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  int gridHorizontalCount = 4;
+  int gridVerticalCount = 4;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Home'));
+    return GestureDetector(
+      onLongPress: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SettingsView()),
+        );
+      },
+      child: Scaffold(
+        body: ListView.builder(
+          itemCount: appsService.apps.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(appsService.apps[index].appName),
+              leading:
+                  appsService.apps[index].appIcon == null
+                      ? null
+                      : SizedBox(
+                        width: 48,
+                        height: 48,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Image.memory(
+                            Uint8List.fromList(
+                              appsService.apps[index].appIcon ?? [],
+                            ),
+                            fit: BoxFit.contain,
+                            filterQuality: FilterQuality.medium,
+                            errorBuilder: (context, error, stackTrace) {
+                              return const Icon(Icons.broken_image);
+                            },
+                          ),
+                        ),
+                      ),
+              onTap: () {
+                AppsHandler.openApp(appsService.apps[index].packageName);
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
