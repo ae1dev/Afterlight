b7dc30da6a36be6f2b4bb7cb9c063c871d3317f5
Elias <elias@deuss.dev>
2025-04-08 13:32:15 +0200

    feat(apps): sort by name

--- lib/services/apps.dart
+++ lib/services/apps.dart
@@ -4,10 +4,14 @@ class AppsService {
   List<AppInfo> apps = [];
 
   Future<void> init() async {
+    // Get apps
     apps = await AppsHandler.getInstalledApps(
       includeSystemApps: false,
       includeAppIcons: true,
       onlyAppsWithLaunchIntent: true,
     );
+
+    // Sort apps by name
+    apps.sort((a, b) => a.appName.compareTo(b.appName));
   }
 }
