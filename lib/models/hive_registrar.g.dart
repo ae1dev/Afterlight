1091a252358fbc193569c21c9b8202f4f40ad6e0
Elias <elias@deuss.dev>
2025-04-12 17:12:15 +0200

    Update hive_registrar.g.dart

--- lib/models/hive_registrar.g.dart
+++ lib/models/hive_registrar.g.dart
@@ -3,12 +3,10 @@
 // Check in to version control
 
 import 'package:hive_ce/hive.dart';
-import 'package:afterlight/models/favorite_app.dart';
 import 'package:afterlight/models/hive_adapters.dart';
 
 extension HiveRegistrar on HiveInterface {
   void registerAdapters() {
-    registerAdapter(FavoriteAppAdapter());
     registerAdapter(FavoriteAppAdapter());
   }
 }
