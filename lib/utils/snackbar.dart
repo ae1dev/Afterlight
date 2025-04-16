1da22c44e991358b4ef17d68456d0eae4c5f0f04
Elias <elias@deuss.dev>
2025-04-16 16:57:08 +0200

    style(snackbar): bold text

--- lib/utils/snackbar.dart
+++ lib/utils/snackbar.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void handleSnackbar(String message, BuildContext context) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: Text(message, style: TextStyle(color: Colors.white)),
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+      ),
       behavior: SnackBarBehavior.floating,
       backgroundColor: Theme.of(context).primaryColor,
     ),
